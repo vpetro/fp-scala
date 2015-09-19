b91d7cfa95449f2cfc5dab138f6f2316ce7692f4
Petro Verkhogliad <vpetro@gmail.com>
2015-09-19 15:58:15 -0400

    Add solution for problem 5.1

---
+++ src/main/scala/chapter5.scala
@@ -0,0 +1,34 @@
+package org.fpscala.chapter5
+
+trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = {
+    @scala.annotation.tailrec
+    def helper(stream: Stream[A], acc: List[A]): List[A] = stream match {
+      case Empty => acc
+      case Cons(h, t) => helper(stream, h() :: acc)
+    }
+    helper(this, List.empty[A])
+  }
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+}
