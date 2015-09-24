dd94f2f72d3e04a99b793837d024614983581635
Petro Verkhogliad <vpetro@gmail.com>
2015-09-24 12:09:27 -0400

    Ad solution for problem 6.1

---
+++ src/main/scala/chapter6.scala
@@ -0,0 +1,24 @@
+package org.fpscala.chapter6
+
+
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+}
+
+object RNG {
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (0 > i) ((i + 1) * (-1)) else i, r)
+  }
+}
