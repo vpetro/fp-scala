625c08a360a137fee700986ebed5f16e4c9c4076
Petro Verkhogliad <vpetro@gmail.com>
2015-10-04 18:55:16 -0400

    Add solution for problem 6.3

--- src/main/scala/chapter6.scala
+++ src/main/scala/chapter6.scala
@@ -26,4 +26,22 @@ object RNG {
     val (i, r) = nonNegativeInt(rng)
     (i.toDouble / Int.MaxValue.toDouble + 1.0, r)
   }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 }
